e70c924c91028da820b64c096e5115f8afd17ca2
Tafarel Yan <tafarel.yan@gmail.com>
2018-01-01 23:35:52 -0200

    Include non-Python template files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def requirements():
         return [install.strip() for install in requirements]
 
 setup(name='t-helpers',
-      version='0.0.5',
+      version='0.0.6',
       description='Tafarel personal helper tools',
       author='Tafarel Yan',
       author_email='tafarel.yan@gmail.com',
