d9e0e749368e5e3e67b571f42d5fd7a91d856312
Tafarel Yan <tafarel.yan@gmail.com>
2018-01-02 00:55:12 -0200

    Dropped requests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='t-helpers',
-      version='0.0.9',
+      version='0.1.0',
       description='Tafarel personal helper tools',
       author='Tafarel Yan',
       author_email='tafarel.yan@gmail.com',
