635e21e1faef6dd1e27a78ee21318e94c27579ad
Tafarel Yan <tafarel.yan@gmail.com>
2018-01-07 20:31:22 -0200

    Rename modules

--- t_helpers/__init__.py
+++ t_helpers/__init__.py
@@ -1,7 +1,8 @@
 import os
 
-from .push_notifications import send_push
-from .telegram_chabots import create_chatbot
+from .push_bullet import send_push_notifications
+from .telegram import create_chatbot
+from .sptrans import *
 
 __all__ = ['send_push', 'create_chatbot']
 
@@ -20,6 +21,10 @@ if not os.path.exists(credentials):
         pb_key = input('Insert PushBullet API KEY: ')
         if pb_key:
             f.write('PUSH_BULLET_API_TOKEN = {}\n'.format(pb_key))
+
+        spt_key = input('Insert SPTrans API KEY: ')
+        if spt_key:
+            f.write('SPTRANS_API_TOKEN = {}\n'.format(spt_key))
 
 with open(credentials, 'r') as f:
     for x in f.read().strip().split('\n'):
---
+++ t_helpers/push_bullet.py
@@ -0,0 +1,21 @@
+import os
+
+import requests
+
+
+def send_push_notifications(title, body):
+    url = 'https://api.pushbullet.com/v2/pushes'
+
+    headers = {
+        'Access-Token': os.environ['PUSH_BULLET_API_TOKEN'],
+    }
+
+    payload = {
+        'type': 'note',
+        'title': title,
+        'body': body,
+    }
+
+    r = requests.post(url, headers=headers, data=payload)
+
+    return r.json()
