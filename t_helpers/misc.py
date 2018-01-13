d83bd387e7cf365b96768484b8e31ee9be588264
Tafarel Yan <tafarel.yan@gmail.com>
2018-01-12 23:26:32 -0200

    Added warnings to misc

--- t_helpers/misc.py
+++ t_helpers/misc.py
@@ -1,7 +1,7 @@
 import os
 import io
-import stat
 import zipfile
+import warnings
 
 import requests
 
@@ -14,8 +14,6 @@ def create_credentials(credentials_path):
         os = input('Which OS (Windows/MacOS/Linux)? ')
         if os:
             f.write('OS = {}\n'.format(os.lower()))
-        else:
-            f.write('OS = linux\n')
 
         pb_key = input('Insert PushBullet API KEY: ')
         if pb_key:
@@ -25,28 +23,45 @@ def create_credentials(credentials_path):
         if spt_key:
             f.write('SPTRANS_API_TOKEN = {}\n'.format(spt_key))
 
+
 def credentials_to_environ(credentials_path):
     with open(credentials_path, 'r') as f:
         for x in f.read().strip().split('\n'):
-            key, value = x.split(' = ')
-            os.environ[key] = value
+            try:
+                key, value = x.split(' = ')
+                os.environ[key] = value
+            except ValueError:
+                pass
+
 
 def install_chromedriver(path=None):
     base_url = 'https://chromedriver.storage.googleapis.com'
 
     version = requests.get(base_url + '/LATEST_RELEASE').json()
 
-    output = {
+    operation_systems = {
         'windows': 'win32',
         'macos': 'mac64',
         'linux': 'linux64'
-    }[os.environ['OS']]
+    }
 
-    file_url = '{0}/{1}/chromedriver_{2}.zip'.format(base_url, version, output)
+    if os.environ.get('OS'):
+        'OS = macos'
+        output = operation_systems[os.environ.get('OS')]
 
-    z = zipfile.ZipFile(io.BytesIO(requests.get(file_url).content))
-    z.extractall(path=path)
+        file_url = '{0}/{1}/chromedriver_{2}.zip'.format(
+            base_url, version, output
+        )
 
-    chromepath = os.path.join(path, 'chromedriver')
-    st = os.stat(chromepath)
-    os.chmod(chromepath, st.st_mode | 0o111)
+        z = zipfile.ZipFile(io.BytesIO(requests.get(file_url).content))
+        z.extractall(path=config_path)
+
+        st = os.stat(chromedriver_path)
+        os.chmod(chromedriver_path, st.st_mode | 0o111)
+    else:
+        message = (
+            'No OS found in your credentials file. '
+            'Please write a new line in your credentials: '
+            'OS = your_operational_system' 
+        )
+        warnings.warn(message, stacklevel=2)
